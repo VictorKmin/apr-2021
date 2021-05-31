25ac69baa17d2ce28df98e3e6ed98a80893a6107
VictorKmin <kminvictor@gmail.com>
2021-05-31 22:17:22 +0300

    Initial commit

---
+++ basic/lesson1/l1.js
@@ -0,0 +1,168 @@
+// let str = "Hello world I am Viktor";
+// let pi = 3.1415;
+// let age = 25;
+// let is_super_admin = false;
+// let isSuperAdmin = false;
+// let undef = undefined;
+// let nulll = null;
+// let obj = {};
+// let s = Symbol('x');
+// let bInt = BigInt('424342323423423423423423423424')
+//
+//
+// let user = {
+//   name: 'Viktor',
+//   age: 25,
+//   car: true,
+//   laptops: ['Dell', 'HP'],
+//   profession: {
+//     title: "Node.js dev",
+//     company: {
+//       name: 'Inoxoft',
+//       address: 'Kuplar 59',
+//     }
+//   }
+// }
+//
+// // alert(user.name);
+// // alert(user.profession.company.address);
+//
+// let names = ['Khrystyna', 'Harry', 'Dimas', 2, false];
+// // let user2 = ['Viktor', 25, true]
+// // alert(user2[2]);
+//
+// let name = 'Viktor';
+// name = {}
+//
+// console.log(name);
+// alert(22);
+// document.write(`
+//     <div>${names[0]}</div>
+//     <hr>
+//     <div>${names[1]}</div>
+//     <hr>
+//     <div>${names[2]}</div>
+//     <hr>
+// `)
+//
+// console.log(3 + 1);
+// console.log('3' + 'HELLO');
+// console.log(3 - 'HELLO');
+//
+// console.log('HELLO' -  ' ');
+//
+// console.log(3 * 2);
+// console.log('3' * '2');
+// console.log('3' + '2');
+// console.log('3' * 'test');
+//
+// console.log(typeof NaN);
+// console.log(typeof 33);
+// console.log(typeof 'Hello');
+// console.log(typeof true);
+// console.log(typeof null);
+
+
+// > < >= <= != == !== ===
+
+// console.log(2 > 3)
+// console.log(2 < 3);
+// console.log('2' < 3);
+// console.log('Andryy' < 'Olag');
+
+// console.log(3 != 3);
+// console.log('3' == 3);
+// console.log('______________')
+// console.log(3 !== 3);
+// console.log('3' === 3);
+//
+// // let age = parseInt(prompt('How old r u?'));
+// // let age2 = parseFloat(prompt('How old r u?'));
+// // let age3 = Number(prompt('How old r u?'));
+// // let age = +prompt('How old r u?');
+// // let name = prompt('Enter name');
+// //
+// //
+// // // if (age > 18) {
+// // //   alert('Welcome');
+// // //   // let numbers = [2, 59, 9, 7, 18];
+// // //   // console.log(numbers);
+// // // } else {
+// // //   if (name === 'admin') {
+// // //     alert('Welcome');
+// // //   } else  {
+// // //     alert('Go to school boy')
+// // //   }
+// // // }
+// //
+// // // || - OR
+// // // && - AND
+// //
+// // if (age > 18 || name === 'admin') {
+// //   alert('Welcome');
+// // } else {
+// //   alert('Go to school boy');
+// // }
+// //
+// // console.log('GO NEXT');
+//
+//
+// let name = prompt('NAME');
+//
+// switch (name) {
+//   case 'Olenka':
+//   case "Anna":
+//     console.log('Hey sister');
+//     break;
+//   case 'Vlad':
+//     console.log('Yo ma man');
+//     break;
+//   case 'Ivan':
+//     console.log('Hey bro');
+//     break;
+//   default:
+//     console.log('WHO R U?')
+// }
+
+
+// const name = 'Viktor'
+//
+// let age = 25;
+// age = 26;
+//
+// if (name === 'Ihor') {
+//   console.log('Go work')
+// } else {
+//   console.log('Go drink')
+// }
+//
+// (name === 'Ihor') ? alert('Go work') : alert('Go drink');
+
+
+// let isOld;
+//
+let age = 899;
+//
+// if (age > 88) {
+//   isOld = true
+// } else {
+//   isOld = false;
+// }
+//
+// console.log(isOld);
+
+console.log(age)
+
+const isOld = (age > 88) ? true : false;
+// const isOld = (age > 88);
+
+age = 22;
+
+
+console.log(isOld);
+console.log(age);
+
+let random = Math.random() * 999;
+let roundedNumber = Math.round(random)
+
+console.log(roundedNumber);
